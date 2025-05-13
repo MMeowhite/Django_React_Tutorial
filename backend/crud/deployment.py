1acbfb98d324c2fcc952580f99d7ac96ac5c4136
Baimiaomiao123 <18723560427@163.com>
2025-05-13 14:12:32 +0800

    deployments

--- backend/crud/deployment.py
+++ backend/crud/deployment.py
@@ -1,11 +1,14 @@
 import os
 from .settings import *
 from .settings import BASE_DIR
+import dj_database_url
  
 # ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] # Allow all hosts for local development
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
+# SECURITY WARNING: keep the secret key used in production secret!
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['ALLOWED_HOSTS']] # Allow all hosts for local development
 DEBUG = False # Disable debug mode for production
+
 
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware', # WhiteNoise middleware for serving static files
@@ -23,16 +26,23 @@ MIDDLEWARE = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # Use WhiteNoise for static files
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Set the static root directory)
 
-connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING'] # Get the connection string from the environment variable
-parameters = {pair.split('='):pair.split('=')[1] for pair in connection_string.split(' ')} # Split the connection string into key-value pairs
+# For Deployment in Azure App Service
+# connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING'] # Get the connection string from the environment variable
+# parameters = {pair.split('='):pair.split('=')[1] for pair in connection_string.split(' ')} # Split the connection string into key-value pairs
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql', # Use PostgreSQL as the database engine
+#         'NAME': parameters['dbname'], # Database
+#         "HOST": parameters['host'], # Database host
+#         "USER": parameters['user'], # Database user
+#         "PASSWORD": parameters['password'], # Database password
+#     }
+
+# }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql', # Use PostgreSQL as the database engine
-        'NAME': parameters['dbname'], # Database
-        "HOST": parameters['host'], # Database host
-        "USER": parameters['user'], # Database user
-        "PASSWORD": parameters['password'], # Database password
-    }
-
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
+
+CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', '').split(',')
