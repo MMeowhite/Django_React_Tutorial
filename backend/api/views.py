4bd156c776737a2571129ad410d75e9be258761a
Baimiaomiao123 <18723560427@163.com>
2025-05-14 10:40:49 +0800

    Final Depolyments

--- backend/api/views.py
+++ backend/api/views.py
@@ -11,139 +11,109 @@ def home(request):
 
 class EmployeesViewSet(viewsets.ViewSet):
     permissions_classes = [permissions.AllowAny]
-    queryset = Employees.objects.all()
     serializer_class = EmployeesSerializer
 
     def list(self, request):
-        queryset = self.queryset
+        queryset = Employees.objects.all()  # 动态获取数据
         serializer = self.serializer_class(queryset, many=True)
         return Response(serializer.data)
+
     def create(self, request):
         serializers = self.serializer_class(data=request.data)
         if serializers.is_valid():
-            serializers.save() # Save the new project to the database
-            return Response(serializers.data, status=201) # Return a 201 Created response with the serialized data
+            serializers.save()  # 保存新数据到数据库
+            return Response(serializers.data, status=201)
         else:
-            return Response(serializers.errors, status=400) # Return a 400 Bad Request response with the validation errors
+            return Response(serializers.errors, status=400)
 
 
 class ProjectManagerViewSet(viewsets.ViewSet):
     permissions_classes = [permissions.AllowAny]
-    queryset = ProjectManager.objects.all()
     serializer_class = ProjectManagerSerializer
 
     def list(self, request):
-        # This method handles GET requests to retrieve a list of projects
-        # It uses the queryset defined in the viewset to get all projects
-        # The queryset is a Django QuerySet object that represents a collection of database records 
-        # The queryset is defined in the viewset and can be filtered or paginated as needed
-        # The serializer_class is a Django REST framework serializer that defines how to convert the queryset into JSON format
-
-        queryset = self.queryset 
-        # You can filter or paginate the queryset here if needed
-        # For example, you can filter by status:
-        # queryset = queryset.filter(status='active')
-        # Or paginate the queryset:   
-        # The many=True argument indicates that we are serializing a list of objects
-        # If you are serializing a single object, you would not use many=True
-        # The serializer will convert the queryset into a JSON response
+        queryset = ProjectManager.objects.all()  # 动态获取数据
         serializer = self.serializer_class(queryset, many=True)
-
-
-        # The serializer.data will return the serialized data in a format that can be returned in the response
-        # The Response object will convert the data into JSON format
-        # and set the appropriate content type for the response
-        # The Response object will automatically set the content type to application/json
-        # The Response object will also handle any errors that occur during serialization
         return Response(serializer.data)
 
     def create(self, request):
         serializers = self.serializer_class(data=request.data)
-        if serializers.is_valid():
-            serializers.save() # Save the new project to the database
-            return Response(serializers.data, status=201) # Return a 201 Created response with the serialized data
-        else:
-            return Response(serializers.errors, status=400) # Return a 400 Bad Request response with the validation errors
-
-    def retrieve(self, request, pk=None):
-        project = self.queryset.get(pk=pk) # Get the project with the given primary key (pk)
-        serializers = self.serializer_class(project)
-        return Response(serializers.data)
-
-    def update(self, request, pk=None):
-        project = self.queryset.get(pk=pk)
-        serializers = self.serializer_class(project, data=request.data)
         if serializers.is_valid():
             serializers.save()
             return Response(serializers.data, status=201)
         else:
             return Response(serializers.errors, status=400)
 
+    def retrieve(self, request, pk=None):
+        try:
+            project = ProjectManager.objects.get(pk=pk)  # 动态获取数据
+            serializers = self.serializer_class(project)
+            return Response(serializers.data)
+        except ProjectManager.DoesNotExist:
+            return Response({'error': 'Project Manager not found'}, status=404)
+
+    def update(self, request, pk=None):
+        try:
+            project = ProjectManager.objects.get(pk=pk)
+            serializers = self.serializer_class(project, data=request.data)
+            if serializers.is_valid():
+                serializers.save()
+                return Response(serializers.data, status=201)
+            else:
+                return Response(serializers.errors, status=400)
+        except ProjectManager.DoesNotExist:
+            return Response({'error': 'Project Manager not found'}, status=404)
+
     def destroy(self, request, pk=None):
         try:
-            project = self.queryset.get(pk=pk)
+            project = ProjectManager.objects.get(pk=pk)
             project.delete()
-            return Response(status=204) # Return a 204 No Content response after deleting the project
-        except Project.DoesNotExist:
-            return Response({'error': 'Project not found'}, status=404) # Return a 404 Not Found response if the project does not exist
+            return Response(status=204)
+        except ProjectManager.DoesNotExist:
+            return Response({'error': 'Project Manager not found'}, status=404)
 
 
 class ProjectViewSet(viewsets.ViewSet):
     permissions_classes = [permissions.AllowAny]
-    queryset = Project.objects.all()
     serializer_class = ProjectSerializer
 
     def list(self, request):
-        # This method handles GET requests to retrieve a list of projects
-        # It uses the queryset defined in the viewset to get all projects
-        # The queryset is a Django QuerySet object that represents a collection of database records 
-        # The queryset is defined in the viewset and can be filtered or paginated as needed
-        # The serializer_class is a Django REST framework serializer that defines how to convert the queryset into JSON format
-
-        queryset = self.queryset 
-        # You can filter or paginate the queryset here if needed
-        # For example, you can filter by status:
-        # queryset = queryset.filter(status='active')
-        # Or paginate the queryset:   
-        # The many=True argument indicates that we are serializing a list of objects
-        # If you are serializing a single object, you would not use many=True
-        # The serializer will convert the queryset into a JSON response
+        queryset = Project.objects.all()  # 动态获取数据
         serializer = self.serializer_class(queryset, many=True)
-
-
-        # The serializer.data will return the serialized data in a format that can be returned in the response
-        # The Response object will convert the data into JSON format
-        # and set the appropriate content type for the response
-        # The Response object will automatically set the content type to application/json
-        # The Response object will also handle any errors that occur during serialization
         return Response(serializer.data)
 
     def create(self, request):
         serializers = self.serializer_class(data=request.data)
-        if serializers.is_valid():
-            serializers.save() # Save the new project to the database
-            return Response(serializers.data, status=201) # Return a 201 Created response with the serialized data
-        else:
-            return Response(serializers.errors, status=400) # Return a 400 Bad Request response with the validation errors
-
-    def retrieve(self, request, pk=None):
-        project = self.queryset.get(pk=pk) # Get the project with the given primary key (pk)
-        serializers = self.serializer_class(project)
-        return Response(serializers.data)
-
-    def update(self, request, pk=None):
-        project = self.queryset.get(pk=pk)
-        serializers = self.serializer_class(project, data=request.data)
         if serializers.is_valid():
             serializers.save()
             return Response(serializers.data, status=201)
         else:
             return Response(serializers.errors, status=400)
 
+    def retrieve(self, request, pk=None):
+        try:
+            project = Project.objects.get(pk=pk)  # 动态获取数据
+            serializers = self.serializer_class(project)
+            return Response(serializers.data)
+        except Project.DoesNotExist:
+            return Response({'error': 'Project not found'}, status=404)
+
+    def update(self, request, pk=None):
+        try:
+            project = Project.objects.get(pk=pk)
+            serializers = self.serializer_class(project, data=request.data)
+            if serializers.is_valid():
+                serializers.save()
+                return Response(serializers.data, status=201)
+            else:
+                return Response(serializers.errors, status=400)
+        except Project.DoesNotExist:
+            return Response({'error': 'Project not found'}, status=404)
+
     def destroy(self, request, pk=None):
         try:
-            project = self.queryset.get(pk=pk)
+            project = Project.objects.get(pk=pk)
             project.delete()
-            return Response(status=204) # Return a 204 No Content response after deleting the project
+            return Response(status=204)
         except Project.DoesNotExist:
-            return Response({'error': 'Project not found'}, status=404) # Return a 404 Not Found response if the project does not exist
+            return Response({'error': 'Project not found'}, status=404)
