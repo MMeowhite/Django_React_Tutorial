5bbbfc2650deec57f45e444dff8d550a8f86d49e
Baimiaomiao123 <18723560427@163.com>
2025-05-14 09:29:29 +0800

    deployments

--- frontend/src/components/Axios.jsx
+++ frontend/src/components/Axios.jsx
@@ -1,14 +1,17 @@
 import axios from "axios";
 
-const baseURL = 'http://127.0.0.1:8000/'
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+// Use environment variables to set the base URL
+const baseURL = import.meta.env.VITE_API_BASE_URL || 'https://djangoreacttutorial-production.up.railway.app/'
+
 const AxiosInstance = axios.create({
     baseURL: baseURL, // Base URL for the API (Django server)
     timeout: 5000, // Set a timeout of 5 seconds for requests
-    header: {
+    headers: {
         'Content-Type': 'application/json', // Set the content type to JSON
         'Accept': 'application/json',   // Accept JSON responses
     }
-
-})
+});
 
 export default AxiosInstance;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the current mode (e.g., development or production)
+  const env = loadEnv(mode, process.cwd());
 
-
-
+  return {
+    plugins: [react()],
+    base: '/',
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL, // 使用加载的环境变量
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  };
+});
